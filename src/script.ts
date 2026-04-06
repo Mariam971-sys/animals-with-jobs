31baf58f2dcd1d27dbbe639030f76289db42426f
mariam <bahjo066@hotmail.com>
2026-04-06 10:53:51 +0200

    script.ts fil

--- src/script.ts
+++ src/script.ts
@@ -1,11 +1,20 @@
-import fetchAnimals from "./modules/fetchAnimals.ts";
-import renderListOfAnimals from "./modules/renderListOfAnimals.ts";
+import { fetchAnimals } from "./modules/fetchAnimals";
+import { renderList } from "./modules/renderListOfAnimals";
+import { renderAnimalInfo } from "./modules/renderAnimalInfo";
 
-async function init() {
+// 1️⃣ Define async init function
+const init = async (): Promise<void> => {
+  // 2️⃣ Fetch animals from API or file
   const animals = await fetchAnimals();
-  if (animals) {
-    renderListOfAnimals(animals);
-  }
-}
 
+  // 3️⃣ Render list of animals in the UL
+  // Pass a callback to renderAnimalInfo when an <li> is clicked
+  if (animals) {
+    renderList(animals, (animal) => {
+      renderAnimalInfo(animal);
+    });
+  }
+};
+
+// 4️⃣ Call init to start everything
 init();
